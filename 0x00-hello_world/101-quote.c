0b74817ba1b6543d67427101542329d800b403da
bolaji <ibrahimkenny17@gmail.com>
2022-03-10 15:43:07 -0800

    task_08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - This should successfully print out the text
  *
  * Return: 1 if there are no errors
- */
+*/
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
