3c6096b507ea6562f1bd2eaf526b4a3d976da7d3
bolaji <ibrahimkenny17@gmail.com>
2022-03-11 03:00:40 -0800

    task_08

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <unistd.h>
-
 /**
- * main - This should successfully print out the text
+ * main - Entry point
  *
- * Return: 1 if there are no errors
-*/
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\", -Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
