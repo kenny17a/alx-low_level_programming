5429acead670e8237b0863845816756f1be13b9c
bolaji <ibrahimkenny17@gmail.com>
2022-04-01 19:47:48 -0700

    task_00

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: gives the size of the array elements
+ *
+ * @argv: gives the elements in the array
+ *
+ * Return: (0) on success.
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+	puts(argv[0]);
+
+	return (0);
+
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Alx project tasks on C program about argc, argv.
