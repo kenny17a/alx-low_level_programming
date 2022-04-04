8495bbbb2796427ac345ddc5d18dceeb54b3934d
bolaji <ibrahimkenny17@gmail.com>
2022-04-04 06:48:50 -0700

    task_0

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Alx projects task on how to create C static library
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _putchar - used to format a char to stdout
+*
+* @c: Holds the character
+*
+* Return: 0 on success
+*/
+int _putchar(char c __attribute__((unused)))
+{
+
+return (0);
+}
