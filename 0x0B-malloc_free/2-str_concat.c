eb0add391815ac01d0fbdf4330a87b1b3783ba1e
bolaji <ibrahimkenny17@gmail.com>
2022-04-05 06:55:53 -0700

    task_02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,45 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
- */
-
-
-int _strlen(char *s)
-{
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
+ * *str_concat - main function.
+ * @s1: Primer string.
+ * @s2: Segundo string.
+ *
+ * Description: Escriba una función que devuelva un puntero a
+ * un espacio recién asignado en la memoria, que contiene
+ * una copia de la cadena dada como parámetro.
+ * Return: The pointer to str.
  */
 
 char *str_concat(char *s1, char *s2)
 {
-int size1, size2, i;
-char *m;
+	int size; /* Tamaño del string*/
+	int size2; /* Tamañano del segundo string */
+	int i, j; /* Iteradores de bucle */
+	char *array; /* Salida */
 
-if (s1 == NULL)
-	s1 = "\0";
-if (s2 == NULL)
-	s2 = "\0";
+	if (!s1)
+		s1 = ""; /* Devuelve un string vacio */
+	if (!s2)
+		s2 = "";
+	for (size = 0; s1[size] != '\0'; size++) /* leng del string */
+		;
+	for (size2 = 0; s2[size2] != '\0'; size2++) /* leng string */
+		;
+	array = malloc(((size) + (size2 + 1)) * sizeof(char)); /* New memory alloc */
 
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-	return (0);
-
-for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		m[i] = s1[i];
-	else
-		m[i] = s2[i - size1];
-}
-m[i] = '\0';
-return (m);
+	if (!array)
+	{
+		return (NULL); /* Devuelve NULLL si malloc falla */
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = s1[i]; /* Copiamos string en base a string dado por main */
+	}
+	for (j = 0; j < (size2 + 1); j++)
+	{
+		array[i + j] = s2[j]; /* Copiamos string en base a string dado por main */
+	}
+	return (array); /* Devolvemos string nuevo */
 }
