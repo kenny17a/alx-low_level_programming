866bee3b431a1b0e01f9a0b6b70e6682065c3ea7
bolaji <ibrahimkenny17@gmail.com>
2022-10-06 02:45:45 -0700

    algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef __SEARCH_ALGOS__H
+#define __SEARCH_ALGOS__H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
